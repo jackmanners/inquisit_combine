192dd8d0518869c86433497fa61b6d4d673cf8a5
jackmanners <JS.Manners@outlook.com>
2023-07-27 17:20:24 +0930

    confirm

--- R/inquisit_combine.R
+++ R/inquisit_combine.R
@@ -29,6 +29,20 @@ inquisit.combine <- function(rootpath, filter = NULL) {
   all_files_raw <- list.files(path = rootpath, pattern = "raw", full.names = TRUE)
   all_files_sum <- list.files(path = rootpath, pattern = "summary", full.names = TRUE)
   
+  # Print the files to be combined
+  cat("Raw data files to be combined:\n")
+  print(all_files_raw)
+  cat("\nSummary data files to be combined:\n")
+  print(all_files_sum)
+  
+  # Ask for user confirmation before proceeding
+  response <- readline(prompt = "Do you want to proceed with the data combination? (y/n): ")
+  
+  if (tolower(response) != "y") {
+    cat("Data combination cancelled.\n")
+    return(NULL)
+  }
+  
   # Read and combine raw data files
   df_raw <- rbindlist(lapply(all_files_raw, function(f) {
     fread(f, sep = "\t")[, file := basename(f)]
